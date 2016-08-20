1cca861f5aa46754e4cdb61a9294749ecee4f362
zhenpanda <zhentzhou@gmail.com>
2016-08-20 18:01:47 -0400

    lockScroll

--- subway.js
+++ subway.js
@@ -8,15 +8,15 @@ window.onload = function() {
 
   var layerTwo = $('#layer_two');
   var layerTwoPos = layerTwo.position().top;
-  $(layerTwo).css('top', layerTwoPos - 30);
+  $(layerTwo).css('top', layerTwoPos - 40);
 
   var layerThree = $('#layer_three');
   var layerThreePos = layerThree.position().top;
-  $(layerThree).css('top', layerThreePos - 15);
+  $(layerThree).css('top', layerThreePos - 20);
 
   var layerFour = $('#layer_four');
   var layerFourPos = layerFour.position().top;
-  $(layerFour).css('top', layerFourPos - 5);
+  $(layerFour).css('top', layerFourPos - 10);
 
   //redefine layer postions
   layerOnePos = layerOne.position().top;
@@ -36,19 +36,28 @@ window.onload = function() {
       */
       var layerOneSpeed = 500/80;
       // console.log(layerOnePos, "layer postion");
-      $(layerOne).css('top', layerOnePos + (ypos/layerOneSpeed));
-      // console.log(ypos/layerOneSpeed);
-      var layerTwoSpeed = 500/30;
-      $(layerTwo).css('top', layerTwoPos + (ypos/layerTwoSpeed));
-      var layerThreeSpeed = 500/15;
-      $(layerThree).css('top', layerThreePos + (ypos/layerThreeSpeed));
-      var layerFourSpeed = 500/5;
-      if (layerFourSpeed) {
-
-      }
-      $(layerFour).css('top', layerFourPos + (ypos/layerFourSpeed));
-
+      var layerOneOffset = (ypos/layerOneSpeed);
       //scroll locks
+      if (layerOneOffset >= 160) {
+        layerOneOffset = 160;
+        // console.log(ypos/layerOneSpeed);
+      }
+      $(layerOne).css('top', layerOnePos + layerOneOffset);
+
+      var layerTwoSpeed = 500/40;
+      var layerTwoOffset = (ypos/layerTwoSpeed);
+      if (layerTwoOffset >= 80) { layerTwoOffset = 80 };
+      $(layerTwo).css('top', layerTwoPos + layerTwoOffset);
+
+      var layerThreeSpeed = 500/15;
+      var layerThreeOffset = (ypos/layerThreeSpeed);
+      if (layerThreeOffset >= 30) { layerThreeOffset = 30 };
+      $(layerThree).css('top', layerThreePos + layerThreeOffset);
+
+      var layerFourSpeed = 500/10;
+      var layerFourOffset = (ypos/layerFourSpeed);
+      if (layerFourOffset >= 20) { layerFourOffset = 20 };
+      $(layerFour).css('top', layerFourPos + layerFourOffset);
 
   });
 
